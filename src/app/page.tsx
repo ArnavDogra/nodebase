b9a641bdda38158d302a0d05e62fd5b6e49c8838
ArnavDogra <arnavdogra3122004@gmail.com>
2026-02-03 19:56:36 +0530

    01-setup

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import {  Button} from "@/components/ui/button";
+
+const Page=() => {
+  return(
+  <div className="min-h-screen min-w-screen flex items-center justify-center"><button>Click me </button></div>);
+
+};
+
+export default Page;
