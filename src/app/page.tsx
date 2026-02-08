4c42f97273088d885fdf7c74f617cc459c3e0bff
ArnavDogra <arnavdogra3122004@gmail.com>
2026-02-08 19:35:56 +0530

    02:database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,8 +2,8 @@ import {  Button} from "@/components/ui/button";
 
 const Page=() => {
   return(
-  <div className="min-h-screen min-w-screen flex items-center justify-center"><button>Click me </button></div>);
+  <div className="min-h-screen min-w-screen flex items-center justify-center"><button>Click me 2 </button></div>);
 
 };
 
-export default Page;
+export default Page; 
